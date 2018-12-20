5b4b50c934b299834b1e56f8e3e9d018fa0dd6a1
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-12-20 16:37:04 +0100

    Add conversion structs

--- cslibs_kdl_conversion/include/cslibs_kdl_conversion/cslibs_kdl_conversion.h
+++ cslibs_kdl_conversion/include/cslibs_kdl_conversion/cslibs_kdl_conversion.h
@@ -1,9 +1,40 @@
 #ifndef CSLIBS_KDL_CONVERSION_H
 #define CSLIBS_KDL_CONVERSION_H
+#include <cslibs_kdl_data/types.h>
 #include <cslibs_kdl_data/vector3.h>
+#include <cslibs_kdl_data/joint_state_data.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/JointState.h>
 #include <kdl/frames.hpp>
 namespace cslibs_kdl {
     KDL::Vector convert(const cslibs_kdl_data::Vector3& v);
+
+    struct HeaderConversion
+    {
+        static void ros2data(const std_msgs::Header& in, cslibs_kdl_data::Header& out);
+        static void data2ros(const cslibs_kdl_data::Header& in, std_msgs::Header& out);
+    };
+
+    struct Vector3Conversion
+    {
+        static void ros2data(const geometry_msgs::Vector3& in, cslibs_kdl_data::Vector3& out);
+    };
+    struct Vector3StampedConversion
+    {
+        static void ros2data(const geometry_msgs::Vector3Stamped& in, cslibs_kdl_data::Vector3Stamped& out);
+    };
+
+    struct JointStateConversion
+    {
+        static void ros2data(const sensor_msgs::JointState& in, cslibs_kdl_data::JointStateData& out);
+        static void data2ros(const cslibs_kdl_data::JointStateData& in, sensor_msgs::JointState& out);
+    };
+
+    struct JointStateStampedConversion
+    {
+        static void ros2data(const sensor_msgs::JointState& in, cslibs_kdl_data::JointStateDataStamped& out);
+        static void data2ros(const cslibs_kdl_data::JointStateDataStamped& in, sensor_msgs::JointState& out);
+    };
 
 }
 #endif // CSLIBS_KDL_CONVERSION_H
--- cslibs_kdl_conversion/src/cslibs_kdl_conversion.cpp
+++ cslibs_kdl_conversion/src/cslibs_kdl_conversion.cpp
@@ -7,3 +7,57 @@ KDL::Vector convert(const cslibs_kdl_data::Vector3& v)
     KDL::Vector res(v(0),v(1),v(2));
     return res;
 }
+
+
+void HeaderConversion::ros2data(const std_msgs::Header& in, cslibs_kdl_data::Header& out)
+{
+    out.frame_id = in.frame_id;
+    out.stamp.fromNSec(in.stamp.toNSec());
+}
+
+void HeaderConversion::data2ros(const cslibs_kdl_data::Header &in, std_msgs::Header &out)
+{
+    out.frame_id = in.frame_id;
+    out.stamp.fromNSec(in.stamp.toNSec());
+}
+
+void Vector3Conversion::ros2data(const geometry_msgs::Vector3& in, cslibs_kdl_data::Vector3& out)
+{
+    out(0) = in.x;
+    out(1) = in.y;
+    out(2) = in.z;
+}
+
+void Vector3StampedConversion::ros2data(const geometry_msgs::Vector3Stamped &in, cslibs_kdl_data::Vector3Stamped& out)
+{
+    HeaderConversion::ros2data(in.header, out.header);
+    Vector3Conversion::ros2data(in.vector, out);
+}
+
+void JointStateConversion::ros2data(const sensor_msgs::JointState &in, cslibs_kdl_data::JointStateData &out)
+{
+    out.names    = in.name;
+    out.position = in.position;
+    out.velocity = in.velocity;
+    out.torque   = in.effort;
+}
+
+void JointStateConversion::data2ros(const cslibs_kdl_data::JointStateData &in, sensor_msgs::JointState &out)
+{
+    out.name     = in.names;
+    out.position = in.position;
+    out.velocity = in.velocity;
+    out.effort   = in.torque;
+}
+
+void JointStateStampedConversion::ros2data(const sensor_msgs::JointState &in, cslibs_kdl_data::JointStateDataStamped &out)
+{
+    HeaderConversion::ros2data(in.header, out.header);
+    JointStateConversion::ros2data(in, out);
+}
+
+void JointStateStampedConversion::data2ros(const cslibs_kdl_data::JointStateDataStamped &in, sensor_msgs::JointState &out)
+{
+    HeaderConversion::data2ros(in.header, out.header);
+    JointStateConversion::data2ros(in.data, out);
+}
